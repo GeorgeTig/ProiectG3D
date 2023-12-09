a368738f34574c2f0c2359ab37c8337be060b849
GeorgeTig <georgecorineltigau@yahoo.com>
2023-12-09 17:14:12 +0200

    created class "Camera" that will help to implement movement, and added its default value

---
+++ Muzeul antipa/Camera.h
@@ -0,0 +1,31 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <math.h>
+
+#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
+
+#include <iostream>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+#pragma once
+
+class Camera
+{
+private:
+    const float zNEAR = 0.1f;
+    const float zFAR = 500.f;
+    const float YAW = -90.0f;
+    const float PITCH = 0.0f;
+    const float FOV = 45.0f;
+    glm::vec3 startPosition;
+    glm::mat4 view;
+};
+
