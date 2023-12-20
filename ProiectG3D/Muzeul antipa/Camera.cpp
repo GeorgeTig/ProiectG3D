4191ba5421971393d2452e595fc87fde21eed01b
GeorgeTig <georgecorineltigau@yahoo.com>
2023-12-20 14:22:31 +0200

    created constructor camera

--- ProiectG3D/Muzeul antipa/Camera.cpp
+++ ProiectG3D/Muzeul antipa/Camera.cpp
@@ -1,5 +1,11 @@
 #include "Camera.h"
 
+Camera::Camera(const int width, const int height, const glm::vec3& position)
+{
+    startPosition = position;
+    Set(width, height, position);
+}
+
 void Camera::MoveForward(float distance)
 {
 	position += forward * distance;
