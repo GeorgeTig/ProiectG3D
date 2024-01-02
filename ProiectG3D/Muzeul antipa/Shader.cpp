fa485fdb7b602d950c6e166aa05c528ca5e74d6a
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-02 21:01:10 +0200

    implemented setter methods

--- ProiectG3D/Muzeul antipa/Shader.cpp
+++ ProiectG3D/Muzeul antipa/Shader.cpp
@@ -30,20 +30,25 @@ unsigned int Shader::GetID()
 
 void Shader::SetInt(const std::string& name, int value) const
 {
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::SetFloat(const std::string& name, float value) const
 {
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::SetVec3(const std::string& name, const glm::vec3& value) const
 {
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
 
 void Shader::SetVec3(const std::string& name, float x, float y, float z) const
 {
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
 }
 
 void Shader::SetMat4(const std::string& name, const glm::mat4& mat) const
 {
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
