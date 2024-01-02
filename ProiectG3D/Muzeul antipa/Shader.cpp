4289aa9b4bd7e1ae737f3a734d1c355413af1b11
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-02 20:57:08 +0200

    added methods in shader.cpp

--- ProiectG3D/Muzeul antipa/Shader.cpp
+++ ProiectG3D/Muzeul antipa/Shader.cpp
@@ -1,1 +1,46 @@
 #include "Shader.h"
+
+void Shader::Init(const char* vertexPath, const char* fragmentPath)
+{
+}
+
+void Shader::CheckCompileErrors(unsigned int shaderStencilTesting, std::string type)
+{
+}
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+}
+
+Shader::~Shader()
+{
+}
+
+void Shader::Use()
+{
+}
+
+unsigned int Shader::GetID()
+{
+	return 0;
+}
+
+void Shader::SetInt(const std::string& name, int value) const
+{
+}
+
+void Shader::SetFloat(const std::string& name, float value) const
+{
+}
+
+void Shader::SetVec3(const std::string& name, const glm::vec3& value) const
+{
+}
+
+void Shader::SetVec3(const std::string& name, float x, float y, float z) const
+{
+}
+
+void Shader::SetMat4(const std::string& name, const glm::mat4& mat) const
+{
+}
