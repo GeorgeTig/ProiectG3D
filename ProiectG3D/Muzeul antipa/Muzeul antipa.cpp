52210309d32e1ee7cf1fab985d1512fe61e75b24
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-04 13:10:21 +0200

    added room class, shadowmapping and obj folders

--- ProiectG3D/Muzeul antipa/Muzeul antipa.cpp
+++ ProiectG3D/Muzeul antipa/Muzeul antipa.cpp
@@ -1,5 +1,38 @@
-#include <iostream>
+#include <stdlib.h> // necesare pentru citirea shaderStencilTesting-elor
+#include <stdio.h>
+#include <math.h> 
 
+#include <GL/glew.h>
+
+#define GLM_FORCE_CTOR_INIT 
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stb_image.h>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+#include "Camera.h"
+#include "Shader.h"
+// settings
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+
+bool rot = false;
+
+Camera* pCamera = nullptr;
 int main()
 {
 	std::cout << "Hello World!\n";
---
+++ ProiectG3D/Muzeul antipa/Room.h
@@ -0,0 +1,19 @@
+
+
+#pragma once
+class Room
+{
+
+public:
+	void renderWall1();
+	void renderWall2();
+	void renderWall3();
+	void renderWall4();
+	void renderWall5();
+	void renderWall6();
+	void renderWall7();
+	void renderWall8();
+	void renderWall9();
+	void renderWall10();
+};
+
