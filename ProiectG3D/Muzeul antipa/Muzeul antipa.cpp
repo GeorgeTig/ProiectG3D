24420ddc84fd402c29633497deecf27f2aab72e8
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-05 16:48:03 +0200

    added objects

--- ProiectG3D/Muzeul antipa/Muzeul antipa.cpp
+++ ProiectG3D/Muzeul antipa/Muzeul antipa.cpp
@@ -35,8 +35,11 @@ objl::Loader Loader;
 Camera* pCamera = nullptr;
 unsigned int planeVAO = 0;
 
+//vertices and indices for the objects
 float vertices[82000];
 unsigned int indices[72000];
+
+// VAO, VBO and EBO for the objects 
 GLuint floorVAO, floorVBO, floorEBO;
 GLuint floorVAO1, floorVBO1, floorEBO1;
 GLuint floorVAO2, floorVBO2, floorEBO2;
@@ -59,8 +62,12 @@ void renderFloor();
 void renderWallRoom(const Shader& shader);
 
 //creating textures
+void renderZebra(const Shader& shader);
+void renderSkeletal(const Shader& shader);
 
 //rendering objects
+void renderZebra();
+void renderSkeletal();
 
 int main(int argc, char** argv)
 {
@@ -117,6 +124,9 @@ int main(int argc, char** argv)
 	// load textures
 	unsigned int wallTexture = CreateTexture(strExePath + "\\wall.jpg");
 	unsigned int floorTexture = CreateTexture(strExePath + "\\floor.jpg");
+	/*unsigned int zebraTexture = CreateTexture(strExePath + "\\zebra.jpg");*/
+	unsigned int SkeletalTexture = CreateTexture(strExePath + "\\zebra.jpg");
+
 
 	// configure depth map FBO
 	const unsigned int SHADOW_WIDTH = 4098, SHADOW_HEIGHT = 4098;
@@ -198,6 +208,28 @@ int main(int argc, char** argv)
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+		/*glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, zebraTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderZebra(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);*/
+
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, SkeletalTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderSkeletal(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -228,6 +260,20 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderWallRoom(shadowMappingShader);
+
+		/*glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, zebraTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderZebra(shadowMappingShader);*/
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, SkeletalTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderSkeletal(shadowMappingShader);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
@@ -518,4 +564,195 @@ void renderWallRoom(const Shader& shader)
 	shader.SetMat4("model", model);
 	renderWall(floorVAO4, floorVBO4, floorEBO4,4);
 
+}
+
+void renderZebra(const Shader& shader)
+{
+	//zebra
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-40.5f, 0.0f, -13.4f));
+	model = glm::scale(model, glm::vec3(0.5f));
+	model = glm::rotate(model, glm::radians(10.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	shader.SetMat4("model", model);
+	renderZebra();
+}
+unsigned int indicesZ[7200000];
+objl::Vertex verZ[2000000];
+
+GLuint zebraVAO, zebraVBO, zebraEBO;
+
+void renderZebra()
+{
+	// initialize (if necessary)
+	if (zebraVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\ZEBRA.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verZ[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesZ[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &zebraVAO);
+		glGenBuffers(1, &zebraVBO);
+		glGenBuffers(1, &zebraEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, zebraVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verZ), verZ, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, zebraEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesZ), &indicesZ, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(zebraVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(zebraVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, zebraVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, zebraEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+unsigned int indicesE[720000];
+objl::Vertex verE[820000];
+
+GLuint elephantVAO, elephantVBO, elephantEBO;
+
+void renderSkeletal()
+{
+	// initialize (if necessary)
+	if (elephantVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\OBJ\\NHMW-Geo-Plateosaurus_low res.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verE[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesE[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &elephantVAO);
+		glGenBuffers(1, &elephantVBO);
+		glGenBuffers(1, &elephantEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, elephantVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verE), verE, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elephantEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesE), &indicesE, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(elephantVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(elephantVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, elephantVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elephantEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+void renderSkeletal(const Shader& shader)
+{
+	//elephant
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-62.5f, 4.0f, -13.0f));
+	model = glm::scale(model, glm::vec3(0.0035f));
+	model = glm::rotate(model, glm::radians(100.0f), glm::vec3(0.0f, 1.0f, 5.0f));
+	shader.SetMat4("model", model);
+	renderSkeletal();
 }
